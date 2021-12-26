4b45d1216ff53bd72ac4edddb69546a3af4cfd14
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2021-12-26 20:44:42 +0530

    lot practice

--- arrays.java
+++ arrays.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class arrays {
     public static void main(String[] args) {
         // we use arrays to store list of objects. we can store any type of object in
@@ -11,15 +14,33 @@ public class arrays {
 
         // another method to create java arrays.
         int[] numbers1 = { 1, 2, 3 };
+
+        // java arrays have a fixed length(size). you cannot add or remove new items
+        // once you initiated an array. If you need to add new items or remove existing
+        // items you have to use one of the collection classes
+
+        Arrays.sort(numbers1);
+
         int var1 = 99;
 
-        System.out.println(numbers);
+        System.out.println(numbers); // you cant print arrays in java as simple as in python or whatever language you
+                                     // have to use a method called .toString comes with array class to convert the
+                                     // array into a string and then print
+        System.out.println(Arrays.toString(numbers)); // now you get the required array printed as simeple as that.
+                                                      // tedious but have to use that way.
+
+        // for a nested arrray the method is more lengthy
+        String[][] deepArray = new String[][] { { "asi", "tha" }, { "divi", "sekara" } };
+        System.out.println(Arrays.toString(deepArray)); // printing the bullshit again
+        System.out.println(Arrays.deepToString(deepArray)); // now its beautifully printed. a nested array
+
         System.out.println("asitha" + "indrajtih" + 10 + 20 + var1); // all the variables converted into strings and
                                                                      // then concatenated with the strings
         System.out.println(10 + 20 + var1); // now the variables were considered as integers and then calculated the
                                             // summation pretty strange haa
         System.out.println(10 + 20 + var1 + "asitha indrajith"); // now even if we put a string at the end we couldnt
                                                                  // stop adding the integers together not as strings
+
         System.out.println(10 + 20 + "asitha indrajith" + 30 + var1); // integers before the string were added and after
                                                                       // the string were considered as strings and
                                                                       // concatednated them
--- sample.java
+++ sample.java
@@ -16,6 +16,66 @@ public class sample {
         Scanner numbers = new Scanner(System.in);
         fnum = numbers.nextDouble(); // you can only input numbers decimal or integers
         System.out.println(fnum);
-    }
 
+        // constants(also called variables) have a fixed valu. Once we set them we
+        // cannot change them.
+        // by convention we use Capital letters to name constants. Multiple words can be
+        // seperated using an underscore.
+
+        // public static final double INTEREST_RATE = 0.04;
+
+        // arithmetic expressions
+        int x = 10 + 3;
+        System.out.println(x);
+
+        // addition
+        int y = x + 2;
+        y++; // this is increment operation
+        System.out.println(y);
+        x++; // this is increment operation
+        System.out.println(x);
+        x--; // this is decrement operation
+        System.out.println(x);
+
+        x = 10 * 3;
+        x = 10 / 3;
+        float var1 = 10 / 3;
+        float var2 = 10 % 3;
+        System.out.println(var1 + "this is division");
+        System.out.println(var2 + "this is modulo division-modulous");
+
+        // this is augmented assignment operator
+        int var3 = 10;
+        var3 += 10;
+        System.out.println(var3);
+
+        // the order of the operations will be BODMAS where prior is division and
+        // multiplication and then addition and subtraction. before all of the
+        // operations prior is
+
+        // In java we have two types of casting
+        // Implicit - happens automatically when we store a value in larger or more
+        // precise data type
+        // Explicit - we do it manually
+        // Implicit casting happens because we try to store a short value (2 bytes) in
+        // an int (4 bytes)
+
+        // following is implicit casting
+        short x_ = 1;
+        int y_ = x_;
+        System.out.println(y_);
+
+        // usually we cant define same varibale name again.
+
+        // following is explicit casting
+        int x_1 = 1;
+        short y_1 = (short) x;
+
+        // to convert a string to a number we can use following methods.
+        byte var4 = Byte.parseByte("1");
+        short var5 = Short.parseShort("1");
+        int var6 = Integer.parseInt("19");
+        System.out.println(var6);
+        System.out.println(var4);
+    }
 }
