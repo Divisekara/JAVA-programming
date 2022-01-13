4cf286da374af1a25cf6350da62254b26b0bd9e0
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-13 18:27:24 +0530

    started hackerank codes in java

---
+++ practice codes/InsertionSort.java
@@ -0,0 +1,33 @@
+
+public class InsertionSort {
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                printArray(arr);
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 22, 14, 10, 5, 1 };
+
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+
+        printArray(arr);
+    }
+}
--- tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-Test!");
-        // double tuna;
-        // tuna = 5.28;
-        // System.out.print("Asitha indrajith"); // no new line
-        // System.out.println(tuna);
